a4f75acd61b45845c35385d2cd2c5e44eccc8c2e
Abhimanyu Pallavi Sudhir <43954672+abhimanyupallavisudhir@users.noreply.github.com>
2019-03-14 12:34:52 +0000

    Useful theorem important for hyperreals

--- src/data/set/basic.lean
+++ src/data/set/basic.lean
@@ -456,6 +456,8 @@ theorem singleton_def (a : α) : ({a} : set α) = insert a ∅ := rfl
 @[simp] theorem mem_singleton_iff {a b : α} : a ∈ ({b} : set α) ↔ a = b :=
 by finish [singleton_def]
 
+lemma set_of_eq_eq_singleton : {n | n = 0} = {0} := set.ext $ λ n, (set.mem_singleton_iff).symm
+
 -- TODO: again, annotation needed
 @[simp] theorem mem_singleton (a : α) : a ∈ ({a} : set α) := by finish
 
